86e226c815a8364eb9ffeb41ce212324a55ed658
Kyle Pence <kpence@Kyles-MacBook-Pro.local>
2019-10-11 10:26:21 -0500

    made more useable

--- text-scroller.py
+++ text-scroller.py
@@ -1,18 +1,28 @@
+#!/usr/bin/python
 import moviepy.editor as mpy
 from mutagen.mp3 import MP3
 from gtts import gTTS 
 import textwrap
 import string
+import sys
 
 language = 'en'
-WHITE = (255, 255, 255)
+TEXT_FILE = './speech.txt' # Text file must be in ASCII format
+BACKGROUND_COLOR = (255, 255, 255)
 VIDEO_SIZE = (1028, 640)
+TEXT_COLUMN_WIDTH = 50
+FONT_FAMILY = 'Xolonium-Bold'
+FONT_COLOR = 'black'
+TEXT_ALIGN = 'West' # Options: West, Center, East
+OUTPUT_FILE = 'video_with_python.webm'
 
-f = open("speech.txt","r")
+# Open the text file    
+f = open(TEXT_FILE,"r")
 origin_txt = f.read()
 
+# Wrap the text around
 wrapper = textwrap.TextWrapper()
-wrapper.width = 50
+wrapper.width = TEXT_COLUMN_WIDTH
 txt_height = -1
 txt = ''
 for pg in origin_txt.splitlines():
@@ -21,44 +31,35 @@ for pg in origin_txt.splitlines():
 txt_height = txt.count('\n')
 txt = 10*"\n" +txt + 10*"\n"
 
+# Fix problems with ASCII code
 printable = set(string.printable)
 audio_txt = [filter(lambda x: x in printable, origin_txt)]
 
-audio_clips = []
-duration = 0
+# Create audio and get audio duration
+myobj = gTTS(text=audio_txt, lang=language, slow=False) 
+myobj.save("audio.mp3")
+audio.append(mpy.AudioFileClip("audio.mp3"))
+duration = MP3("audio.mp3").info.length
 
-for i in range(len(audio_txt)):
-    myobj = gTTS(text=audio_txt[i], lang=language, slow=False) 
-    myobj.save("audio"+str(i)+".mp3")
-    audio_clips.append(mpy.AudioFileClip("audio"+str(i)+".mp3"))
-    duration += MP3("audio"+str(i)+".mp3").info.length
+# Create the Text clip
+text = mpy.TextClip(txt,color=FONT_COLOR, align='West',fontsize=26,
+                    font=FONT_FAMILY, method='label')
 
-audio = mpy.concatenate_audioclips(audio_clips)
-
-
-
-text = mpy.TextClip(txt,color='black', align='West',fontsize=26,
-                    font='Xolonium-Bold', method='label')
-
-
-# duration per line
+# Scroll the text at the right speed
 line_height = 30
 txt_speed = float(line_height) * float(txt_height) / float(duration)
-print(txt_speed)
 
 fl = lambda gf,t : gf(t)[int(txt_speed*t):int(txt_speed*t)+VIDEO_SIZE[1],:]
 moving_txt= text.fl(fl, apply_to=['mask'])
 
-
+# Create the video clip
 clip = mpy.CompositeVideoClip(
     [
         moving_txt.set_position('center')
     ],
     size=VIDEO_SIZE).\
     on_color(
-        color=WHITE,
+        color=BACKGROUND_COLOR
         col_opacity=1).set_duration(duration).set_audio(audio)
 
-print(txt_height)
-print(txt)
-clip.write_videofile('video_with_python.webm', fps=10)
+clip.write_videofile(OUTPUT_FILE, fps=10)
